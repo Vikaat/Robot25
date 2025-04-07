22964ffdcc686ef0b9c9ea1f2096caaaeed39fc6
Srivikaat (Vikaat) Ravi <vikaat08@gmail.com>
2025-04-07 01:00:49 -0700

    Update BuildConstants.java

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot25";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 290;
-  public static final String GIT_SHA = "6ff4b6bcfcb50ffce26addcf1a1749534c679475";
-  public static final String GIT_DATE = "2025-04-07 00:36:46 PDT";
+  public static final int GIT_REVISION = 291;
+  public static final String GIT_SHA = "9c1d3c86c1c7c41d88264cc583605eeeb99b4f27";
+  public static final String GIT_DATE = "2025-04-07 00:56:35 PDT";
   public static final String GIT_BRANCH = "off-season-testing-v2";
-  public static final String BUILD_DATE = "2025-04-07 00:55:33 PDT";
-  public static final long BUILD_UNIX_TIME = 1744012533918L;
+  public static final String BUILD_DATE = "2025-04-07 01:00:20 PDT";
+  public static final long BUILD_UNIX_TIME = 1744012820045L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
