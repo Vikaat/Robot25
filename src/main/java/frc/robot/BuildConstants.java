36ec72e5e7b9eef04f9b2e74db7d12c95db78724
Srivikaat (Vikaat) Ravi <vikaat08@gmail.com>
2025-04-09 09:04:41 -0700

    Autos + Camera constants added

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot25";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 294;
-  public static final String GIT_SHA = "5756d614019dc828786a1007ceb6ac81e771e848";
-  public static final String GIT_DATE = "2025-04-08 00:20:35 PDT";
+  public static final int GIT_REVISION = 295;
+  public static final String GIT_SHA = "a0549b512d0a6ddef440193c0bd9108bc0ceef47";
+  public static final String GIT_DATE = "2025-04-08 23:56:30 PDT";
   public static final String GIT_BRANCH = "off-season-testing-v2";
-  public static final String BUILD_DATE = "2025-04-08 23:55:54 PDT";
-  public static final long BUILD_UNIX_TIME = 1744181754993L;
+  public static final String BUILD_DATE = "2025-04-09 09:00:54 PDT";
+  public static final long BUILD_UNIX_TIME = 1744214454042L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
